a2a96e3b496a3338c2143f1388be714103dd9bc5
bintangandk <bintangsmnp@gmail.com>
2023-11-24 19:27:50 +0700

    memperbaiki upload surat peminjaman

---
+++ app/Models/Peminjaman.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Peminjaman extends Model
+{
+    use HasFactory;
+
+    protected $table = 'peminjamans';
+
+    protected $fillable = [
+        'ruang_id',
+        'nama_peminjam',
+        'tanggal_peminjaman',
+        'tanggal_pengembalian',
+        'surat',
+    ];
+
+    public function ruang()
+    {
+        return $this->belongsTo(Ruang::class);
+    }
+}
