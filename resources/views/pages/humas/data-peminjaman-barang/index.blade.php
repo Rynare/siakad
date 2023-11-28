2aca3d12801bab92f656cece3fafa16116566413
bintangandk <bintangsmnp@gmail.com>
2023-11-28 11:48:53 +0700

    edit peminjaman barang

--- resources/views/pages/humas/data-peminjaman-barang/index.blade.php
+++ resources/views/pages/humas/data-peminjaman-barang/index.blade.php
@@ -13,37 +13,6 @@ Data Peminjaman barang
 @section('content')
 {{-- @dd($peminjaman) --}}
 
-<!--<div class="row">
-    <div class="col-3">
-        <div class="card my-4">
-            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-success shadow-primary border-radius-lg pt-4 pb-3">
-                    <h6 class="text-white text-capitalize ps-3">Jumlah Barang 
-                </div>
-            </div>
-            <div class="card-body px-0 pb-2">
-                <div class="table-responsive pb-2 px-3">
-                    <img src="{{ asset('storage/public/img/c.jpg') }}" alt="barang">
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="col-3">
-        <div class="card my-4">
-            <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-success shadow-primary border-radius-lg pt-4 pb-3">
-                    <h6 class="text-white text-capitalize ps-3">Jumlah Ruang 
-                </div>
-            </div>
-            <div class="card-body px-0 pb-2">
-                <div class="table-responsive pb-2 px-3">
-                    <img src="{{ asset('storage/public/img/c.jpg') }}" alt="ruang">
-                </div>
-            </div>
-        </div>
-    </div>
-</div>-->
-
 <div class="row">
     <div class="col-12">
         @if ($hariini->count())
