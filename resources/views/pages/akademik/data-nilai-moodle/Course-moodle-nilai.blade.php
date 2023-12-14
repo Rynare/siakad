3a02b394fa302dbd1927be9c401a6fbbfa7114c1
Izulfirman <mohizulfirman@gmail.com>
2023-12-14 17:00:29 +0700

    Update Course-moodle-nilai.blade.php

--- resources/views/pages/akademik/data-nilai-moodle/Course-moodle-nilai.blade.php
+++ resources/views/pages/akademik/data-nilai-moodle/Course-moodle-nilai.blade.php
@@ -35,51 +35,60 @@
         $userGradesData = $gradeItems['usergrades'];
     @endphp
 
-
-    {{-- Iterate through usergrades --}}
-    @foreach ($userGradesData as $grade)
-        @if (strpos(strtolower($grade['userfullname']), 'pengajar') === false)
-            {{-- Check if search query matches userfullname --}}
-            @if (isset($_GET['search']) && stripos($grade['userfullname'], $_GET['search']) === false)
-                @continue
-            @endif
-
-            <div class="row mt-3">
-                <div class="col-12">
-                    <!-- Card for User Fullname -->
-                    <div class="card mb-1" style="background-color: #ffffff; color: #ffffff; font-size: px; border-radius: 14px;">
-                        <div class="card-body">
-                            <h4 class="card-title" style="font-size: 20px;">{{ $grade['userfullname'] }}</h4>
-                            <!-- Iterate through gradeitems -->
-                            @foreach ($grade['gradeitems'] as $item)
-                                <!-- Check if itemmodule is quiz or assign -->
-                                @if (in_array($item['itemmodule'], ['quiz', 'assign']))
-                                    <!-- Card for Item Name -->
-                                    <div class="card mb-2" style="background-color: #ffffff; border-radius: 6px;">
-                                        <div class="card-body">
-                                            <h5 class="card-title" style="font-size: 14px; margin-bottom: 0;">{{ $item['itemname'] }}</h5>
-                                            <ul class="list-group list-group-flush">
-                                                <li class="list-group-item" style="font-size: 14px; padding: 1px;">Nilai: {{ $item['graderaw'] }}</li>
-                                                <!-- Check if items key exists before looping -->
-                                                @if (array_key_exists('items', $item))
-                                                    <!-- Loop untuk menampilkan data lainnya -->
-                                                    @foreach ($item['items'] as $column)
-                                                        <li class="list-group-item" style="font-size: 14px; padding: 1px;">{{ $column['column1'] }}: {{ $column['column2'] }}</li>
-                                                    @endforeach
-                                                @endif
-                                            </ul>
-                                        </div>
-                                    </div>
-                                    <!-- End of Card for Item Name -->
-                                @endif
-                            @endforeach
-                        </div>
-                    </div>
-                    <!-- End of Card for User Fullname -->
-                </div>
+    {{-- Check if $userGradesData is empty --}}
+    @if (empty($userGradesData))
+        <div class="row mt-5 text-center">
+            <div class="col-12">
+                <p style="font-size: 24px; color: #000;">Belum Memiliki Tugas</p>
             </div>
-        @endif
-    @endforeach
+        </div>
+    @else
+        {{-- Iterate through usergrades --}}
+        @foreach ($userGradesData as $grade)
+            @if (strpos(strtolower($grade['userfullname']), 'pengajar') === false)
+                {{-- Check if search query matches userfullname --}}
+                @if (isset($_GET['search']) && stripos($grade['userfullname'], $_GET['search']) === false)
+                    @continue
+                @endif
+
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <!-- Card for User Fullname -->
+                        <div class="card mb-1" style="background-color: #ffffff; color: #ffffff; font-size: px; border-radius: 14px;">
+                            <div class="card-body">
+                                <h4 class="card-title" style="font-size: 20px;">{{ $grade['userfullname'] }}</h4>
+                                <!-- Iterate through gradeitems -->
+                                @foreach ($grade['gradeitems'] as $item)
+                                    <!-- Check if itemmodule is quiz or assign -->
+                                    @if (in_array($item['itemmodule'], ['quiz', 'assign']))
+                                        <!-- Card for Item Name -->
+                                        <div class="card mb-2" style="background-color: #ffffff; border-radius: 6px;">
+                                            <div class="card-body">
+                                                <h5 class="card-title" style="font-size: 14px; margin-bottom: 0;">{{ $item['itemname'] }}</h5>
+                                                <ul class="list-group list-group-flush">
+                                                    <li class="list-group-item" style="font-size: 14px; padding: 1px;">Nilai: {{ $item['graderaw'] }}</li>
+                                                    <!-- Check if items key exists before looping -->
+                                                    @if (array_key_exists('items', $item))
+                                                        <!-- Loop untuk menampilkan data lainnya -->
+                                                        @foreach ($item['items'] as $column)
+                                                            <li class="list-group-item" style="font-size: 14px; padding: 1px;">{{ $column['column1'] }}: {{ $column['column2'] }}</li>
+                                                        @endforeach
+                                                    @endif
+                                                </ul>
+                                            </div>
+                                        </div>
+                                        <!-- End of Card for Item Name -->
+                                    @endif
+                                @endforeach
+                            </div>
+                        </div>
+                        <!-- End of Card for User Fullname -->
+                    </div>
+                </div>
+            @endif
+        @endforeach
+    @endif
+
     <script>
         document.addEventListener("DOMContentLoaded", function () {
             var searchInput = document.getElementById('search');
@@ -98,3 +107,4 @@
         <!-- Your existing iteration code... -->
     @endforeach
 @endsection
+
