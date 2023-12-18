5c04597c9bd6ae58b9e92e276215c3c657e921ce
Kimi Dandy <kimidandy2004@gmail.com>
2023-12-18 14:12:05 +0700

    p

--- resources/views/pages/akademik/absensi/absensi-admin.blade.php
+++ resources/views/pages/akademik/absensi/absensi-admin.blade.php
@@ -15,7 +15,7 @@
       {{-- Header absensi start--}}
       <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
         <div class="bg-gradient-primary shadow-primary border-radius-lg pt-4 pb-3">
-            <h6 class="text-white text-capitalize ps-3">Absensi</h6>
+            <h6 class="text-white text-capitalize ps-3">Absensi Admin</h6>
         </div>
     </div>
     {{-- Header absensi end --}}
